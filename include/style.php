41254825c621e53bcc7e04107159eaf825cc361a
Nam [Sirirat] <sirirat405@gmail.com>
2025-04-27 20:04:53 +0700

    fix(about-us-history): update file

--- include/style.php
+++ include/style.php
@@ -4,13 +4,15 @@
 <link rel="stylesheet" href="public/assets/lib/swiper-11.1.9/swiper.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
 <link rel="stylesheet" href="public/assets/lib/aos-2.3.1/aos.min.css" />
+<link rel="stylesheet" href="public/assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.css" />
+<link rel="stylesheet" href="public/assets/lib/vanilla-datepicker/dist/css/datepicker.min.css">
 <link href="public/assets/lib/dflip/css/dflip.min.css" rel="stylesheet" type="text/css">
 <link href="public/assets/lib/dflip/css/themify-icons.min.css" rel="stylesheet" type="text/css">
 
 <link rel="stylesheet" href="public/assets/app/css/grids.css?=v<?=time()?>" />
 <link rel="stylesheet" href="public/assets/app/css/style.css?=v<?=time()?>" />
 <link rel="stylesheet" href="public/assets/app/css/navs.css?=v<?=time()?>" />
+<link rel="stylesheet" href="public/assets/app/css/nav-system.css?v=<?= time() ?>" />
 <link rel="stylesheet" href="public/assets/app/css/blocks.css?=v<?=time()?>" />
-<link rel="stylesheet" href="public/assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.css" />
 
 <link rel="stylesheet" id="css-theme" href="public/assets/app/css/color-0.css?=v<?=time()?>" />
